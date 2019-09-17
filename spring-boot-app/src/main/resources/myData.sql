b84b31719e18e27c54cb718e0d4ca5cebfab6580
saurabhdev <singhalsaurabh20@gmail.com>
2019-09-17 14:26:28 +0530

    Added the files

---
+++ spring-boot-app/src/main/resources/myData.sql
@@ -0,0 +1,4 @@
+insert into employee(name) values ('saurabh');
+insert into employee(name) values ('Devops');
+insert into employee(name) values ('AWS');
+insert into employee(name) values ('Java');
